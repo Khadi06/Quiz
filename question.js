f06bc1b313bec9ede98cec326cbfee896b4b038e
Bello Khadijah Omobolanle <abdulsalamkhadijahh@gmail.com>
2020-05-07 17:36:06 +0100

    first commit

---
+++ question.js
@@ -0,0 +1,41 @@
+questions =[{
+    "question" : "Which group of microorganism does Coronavirus belong to?",
+    "option1" : "Fungi",
+    "option2" : "Bacteria",
+    "option3" : "Nematodes",
+    "option4" : "Viruses",
+
+}, {
+    
+    "question" : "Coronavirus was first detected in?",
+    "option1" : "Belgium",
+    "option2" : "Kenya",
+    "option3" : "China",
+    "option4" : "Brazil",
+    
+}, {
+    
+    "question" : "Coronavirus has a _____  genome?",
+    "option1" : "DDA",
+    "option2" : "DNA",
+    "option3" : "RNA",
+    "option4" : "RDA",
+     
+
+}, {
+    
+    "question" : "Symptoms of Coronavirus disease include the following except?",
+    "option1" : "fever",
+    "option2" : "dry cough",
+    "option3" : "breathlessness",
+    "option4" : "back ache",
+
+}, {
+    
+    "question" : "The first epicenter of the Coronavirus disease was in?",
+    "option1" : "Wuhan",
+    "option2" : "New York",
+    "option3" : "Lagos",
+    "option4" : "Toronto",
+
+}]
